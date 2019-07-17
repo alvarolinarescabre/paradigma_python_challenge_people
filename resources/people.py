a6321a33f9b6497d9db2b152085cbdeea081c201
alvarolinarescabre <alvarolinarescabre@gmail.com>
2019-07-17 21:49:41 +0200

    Add check of ID or Name in PUT action

--- resources/people.py
+++ resources/people.py
@@ -15,7 +15,7 @@ class Person(Resource):
         json = [marshal(people, people_fields) for people in People.query.order_by(People.id).all()]
         return jsonify(json)
     
-    def post(self):
+    def post(self):      
         parser = reqparse.RequestParser()
         parser.add_argument('name', location='json', required = True)
         parser.add_argument('is_alive', location='json', required = True)
@@ -31,7 +31,7 @@ class Person(Resource):
         if create_people:
             return json.dumps({'success':True}), 201, {'ContentType':'application/json'} 
         else:
-            abort(404, 'Not Found')
+            abort(400, 'Bad Request')
     
 class PeopleById(Resource):
     def get(self, id):
@@ -45,28 +45,39 @@ class PeopleById(Resource):
         else:
             abort(404, 'Not Found')
 
-    def put(self, id):
-        id_parser = reqparse.RequestParser()
-        id_parser.add_argument('id', location='view_args', required = True)
-        id_args = id_parser.parse_args()
+    def put(self, id):        
+        get_parser = reqparse.RequestParser()
+        get_parser.add_argument('id', location='view_args', required = True)
+        get_parser.add_argument('name', location='json', required = True)
+        get_args = get_parser.parse_args()
         
-        person_parser = reqparse.RequestParser()
-        person_parser.add_argument('name', location='json', required = True)
-        person_parser.add_argument('is_alive', 't')
-        person_parser.add_argument('place_id', location='json', required = True)
-        person_args = person_parser.parse_args()        
-        
-        update_person = People.query.get(id_args['id'])
-        update_person.name = person_args['name']
-        update_person.is_alive = person_args['is_alive']
-        update_person.place_id = person_args['place_id']
+        get_people_id = db.session.query(People.id).filter(People.id == get_args['id'])
+        people_id = get_people_id.first()
 
-        db.session.commit()
-               
-        if update_person:
-            return json.dumps({'success':True}), 201, {'ContentType':'application/json'} 
+        get_people_name = db.session.query(People.name).filter(People.name == get_args['name'])
+        people_name = get_people_id.first()
+
+        if people_id or people_name:
+            person_parser = reqparse.RequestParser()
+            person_parser.add_argument('name', location='json', required = True)
+            person_parser.add_argument('is_alive', 't')
+            person_parser.add_argument('place_id', location='json', required = True)
+            person_args = person_parser.parse_args()        
+            
+            update_person = People.query.get(get_args['id'])
+            update_person.name = person_args['name']
+            update_person.is_alive = person_args['is_alive']
+            update_person.place_id = person_args['place_id']
+
+            db.session.commit()
+            
+            if update_person:
+                return json.dumps({'success':True}), 201, {'ContentType':'application/json'} 
+            else:
+                abort(400, 'Bad Request')
         else:
             abort(404, 'Not Found')
+        
             
     def delete(self, id):
         id_parser = reqparse.RequestParser()
